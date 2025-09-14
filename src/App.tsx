996134bc4549bfbde03ec58246278842c75e8664
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-09-14 19:24:29 +0300

    ud

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => {
     <>
       <Router>
         <nav style={{ padding: '1rem' }}>
-          <Link to="/">Home</Link> |{" "}
+          <Link to="/home">Home</Link> |{" "}
           {!isAuthenticated ? (
               <>
                 <Link to="/login">Login</Link> |{" "}
@@ -76,9 +76,8 @@ const App: React.FC = () => {
         </nav>
   
         <Routes>
-          <Route path="/" element={<Home />}
-          >
-            <Route path="todo" element={<TodoList />} />
+          <Route path="/home" element={<Home />}>
+            <Route path="todo/" element={<TodoList />} />
           </Route>
           <Route path="/about/:aboutId" element={<About />} >
             <Route path="about-me/:aboutMeId" element={<AboutMe />} />
