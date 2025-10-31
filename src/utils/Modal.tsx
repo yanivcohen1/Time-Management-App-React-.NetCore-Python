6f060dd2b31840f901a8613572732111c80ff1bc
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-10-31 13:57:41 +0200

    ud

--- src/utils/Modal.tsx
+++ src/utils/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from 'react';
+import { useTheme } from '../hooks/useTheme';
 
 // Define the interface for your specific props
 interface ModalProps {
@@ -8,12 +9,15 @@ interface ModalProps {
 
 // Combine your props with the `PropsWithChildren` utility type
 const MyModal: React.FC<PropsWithChildren<ModalProps>> = ({ isOpen, onClose, children }) => {
+    const theme = useTheme();
+    const isDarkTheme = theme === 'dark';
+
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="modal-close-button" onClick={onClose}>
+            <div className={`modal-content ${isDarkTheme ? 'modal-content-dark' : ''}`} onClick={(e) => e.stopPropagation()}>
+                <button className={`modal-close-button ${isDarkTheme ? 'modal-close-button-dark' : ''}`} onClick={onClose}>
                     &times;
                 </button>
                 {children} {/* This is where the projected content is rendered */}
